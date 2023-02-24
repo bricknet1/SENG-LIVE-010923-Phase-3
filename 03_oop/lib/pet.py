9b5a2bf90805a345c5cab97a82b5aaef973467f1
Nick Johnson <nlj222@gmail.com>
2023-02-23 17:52:08 -0800

    saving

--- 03_oop/lib/pet.py
+++ 03_oop/lib/pet.py
@@ -10,6 +10,7 @@ import ipdb
 
     # Note: Add 'pass' to the Pet class 
 
+
 # 2. ✅ Instantiate a few Pet instances
 
     # Compare the Pet instances. Are each of them the same object?
@@ -23,6 +24,7 @@ import ipdb
     # Update attributes with new values
 
 # Instance Methods
+
 
 # 4. ✅ Create a "print_pet_details" function that will print each Pet instance's 
 # attributes
@@ -51,3 +53,47 @@ import ipdb
 
     # Use property() to compile get_name / set_name and invoke them
     # whenever we access an Owner instance's name
+class Pet:
+    def __init__(self, name, age, breed, temperament):
+        self.name = name
+        self.age = age
+        self.breed = breed
+        self.temperament = temperament
+
+    def say_hello(self):
+        print("Hello!")
+
+    def print_pet_details(self):
+        print(f'''
+            name: {self.name}
+            age: {self.age}
+            breed: {self.breed}
+            temperament: {self.temperament}
+        ''')
+
+boogie = Pet("Boogie", 8, "Mutt", "Goodest Boy")
+boogie.say_hello()
+boogie.print_pet_details()
+
+class Owner:
+    def __init__(self, age):
+        self.age = age
+        # self.name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self,name):
+        print("setting name")
+        if isinstance(name, str):
+            self._name = name
+        else:
+            print("name must be a string")
+
+    name = property(get_name,set_name)
+
+nick = Owner(37)
+nick.name = 4
+print(nick.name)
+nick.name = "Nick"
+print(nick.name)
